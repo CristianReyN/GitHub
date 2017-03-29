c3ece6663b4a5d7199d966da7bd5859194e1d7c3
DEV\CRey <DEV\CRey>
2017-03-29 17:46:49 -0400

    Moving to new location

---
+++ AutoFeedCompress/ExecuteCompression/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ExecuteCompression
+{
+  static class Program
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(true);
+      //Application.Run(new Form1());
+
+      AutoFeedCompress.ClassCompress clsCompress = new AutoFeedCompress.ClassCompress();
+
+      clsCompress.StartProcess(true, 0);
+    }
+  }
+}
