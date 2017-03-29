3744a01eebdc73e77fdfc94cbdb2e1d712991173
DEV\CRey <DEV\CRey>
2017-03-29 17:45:16 -0400

    Moving to new location.

---
+++ AutoFeedCompress/AutoFeedCompress/ClassCompress.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.IO.Compression;
+
+namespace AutoFeedCompress
+{
+  public class ClassCompress
+  {
+    ClassDB clsData = new ClassDB();
+
+    public void StartProcess(bool blnCompress, int intFID)
+    {
+      if (blnCompress == true)
+      {
+        SqlDataReader dr = null;
+
+        if (intFID == 0)
+          dr = clsData.GetFulfillmentIDs();
+        else
+          dr = clsData.GetFulfillmentIDsByFID(intFID);
+
+        while (dr.Read())
+        {
+          Execute(Convert.ToInt32(dr["FulfillmentID"]), intFID);
+        }
+
+        dr.Close();
+        dr.Dispose();
+        dr = null;
+      }
+      else
+      {
+        DeCompressToFile(2690);
+      }
+    }
+
+    private void Execute(int intFulfillmentID, int intFID)
+    {
+      try
+      {
+        if ((intFID == 0) && (intFulfillmentID > 0))
+        {
+          byte[] data;
+          //byte[] cmpData;
+
+          //create temp zip file
+          // OnMessage("Reading file to memory");
+          //128073--423
+          //128315--linkedin
+          SqlDataReader dr = clsData.GetFromDatabase(intFulfillmentID, CommandBehavior.CloseConnection); //File.ReadAllBytes(@"D:\CRey\from me\fulfilled.txt");
+
+          dr.Read();
+
+          data = (byte[])dr["datasent"];
+
+          // OnMessage("Zipping file to memory");
+          byte[] compressedData = CompressData(data);
+          //**File.WriteAllBytes(@"d:\crey\from me\compressed.zip", compressedData);
+          // OnMessage("Saving file to database");
+          clsData.SaveToDatabase(compressedData, intFulfillmentID);
+        }
+        else
+        {
+          clsData.CleanUpFulfillmentByID(intFulfillmentID);
+        }
+      }
+      catch
+      {
+      }
+    }
+
+    private static byte[] CompressData(byte[] data)
+    {
+      var output = new MemoryStream();
+
+      using (Stream fs = new System.IO.MemoryStream(data))
+      using (var gzip = new GZipStream(output, CompressionMode.Compress, true))
+      {
+        byte[] buffer = new byte[1024];
+        int nRead;
+        while ((nRead = fs.Read(buffer, 0, buffer.Length)) > 0)
+        {
+          gzip.Write(buffer, 0, nRead);
+        }
+
+        /*
+        gzip.Write(data, 0, data.Length);
+        gzip.Close();
+         */
+      }
+      return output.ToArray();
+    }
+
+    private void DeCompressToFile(int intFulfillmentID)
+    {
+      SqlDataReader dr = clsData.GetCompressedFileData(intFulfillmentID);
+      dr.Read();
+      byte[] data = (byte[])dr["CompressedDataSent"];
+
+      byte[] decompressed = Decompress(data);
+
+      File.WriteAllBytes(@"d:\crey\from me\update.txt", decompressed);
+    }
+
+    private static byte[] Decompress(byte[] data)
+    {
+      var output = new MemoryStream();
+      var input = new MemoryStream();
+      input.Write(data, 0, data.Length);
+      input.Position = 0;
+
+      using (var gzip = new GZipStream(input, CompressionMode.Decompress, true))
+      {
+        var buff = new byte[1024];
+        var read = gzip.Read(buff, 0, buff.Length);
+
+        while (read > 0)
+        {
+          output.Write(buff, 0, read);
+          read = gzip.Read(buff, 0, buff.Length);
+        }
+
+        gzip.Close();
+      }
+      return output.ToArray();
+    }
+  }
+}
---
+++ AutoFeedCompress/AutoFeedCompress/ClassDB.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace AutoFeedCompress
+{
+  public class ClassDB
+  {
+    private string _MasterSource = string.Empty;
+
+    public ClassDB()
+    {
+      _MasterSource = "Persist Security Info=False;Integrated Security=SSPI;database=Autofeeds;server=10.10.140.167;Connect Timeout=30";
+    }
+
+    public void SaveToDatabase(byte[] p_Data, int p_intFulfillmentID)
+    {
+      SqlCommand cmm = new SqlCommand();
+      using (SqlConnection cnn = new SqlConnection(_MasterSource))
+      {
+        try
+        {
+          cnn.Open();
+
+          cmm.Connection = cnn;
+          cmm.CommandType = CommandType.StoredProcedure;
+          cmm.CommandText = "dbo.WriteCompressedData";
+          cmm.CommandTimeout = 1000;
+          cmm.Parameters.Add("@FulfillmentID", SqlDbType.Int, -1).Value = p_intFulfillmentID;
+          cmm.Parameters.Add("@CompressedDataSent", SqlDbType.VarBinary, -1).Value = p_Data;
+          cmm.ExecuteNonQuery();
+        }
+        finally
+        {
+          // No need to clean up  the connection since we are using 'using'.
+
+          if (cmm != null)
+          {
+            cmm.Dispose();
+          }
+
+          cmm = null;
+        }
+      }
+      /*
+      using (var cmd = Conn.CreateCommand())
+      {
+        cmd.CommandText = @"MergeFileUploads";
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.Parameters.AddWithValue("@File", data);
+        cmd.Parameters["@File"].DbType = DbType.Binary;
+
+        cmd.Parameters.Add("@SourceField");
+        var parameter = cmd.Parameters["@SourceField"];
+        parameter.DbType = DbType.Int32;
+        parameter.Direction = ParameterDirection.Output;
+
+        cmd.ExecuteNonQuery();
+        sourceFileId = (int)parameter.Value;
+      }
+      */
+    }
+
+    public SqlDataReader GetFulfillmentIDsByFID(int p_intFID)
+    {
+      SqlCommand cmm = new SqlCommand();
+      SqlDataReader dr = null;
+      SqlConnection cnn = new SqlConnection(_MasterSource);
+
+      bool blnErrorOccurred = false;
+
+      try
+      {
+        cnn.Open();
+
+        cmm.Connection = cnn;
+        cmm.CommandType = CommandType.StoredProcedure;
+        cmm.CommandText = "dbo.GetFulfillmentIDsToCleanUp";
+        cmm.Parameters.Add("@FID", SqlDbType.Int, -1).Value = p_intFID;
+
+        cmm.CommandTimeout = 1000;
+        dr = cmm.ExecuteReader(CommandBehavior.CloseConnection);
+
+        return dr;
+      }
+      catch (Exception ex)
+      {
+        blnErrorOccurred = true;
+
+        throw ex;
+      }
+      finally
+      {
+        if (blnErrorOccurred == true)
+        {
+          if (cnn != null)
+          {
+            if (cnn.State == ConnectionState.Open)
+            {
+              cnn.Close();
+            }
+          }
+
+          cnn = null;
+
+          if (!dr.IsClosed)
+          {
+            dr.Close();
+          }
+
+          dr = null;
+        }
+
+        if (cmm != null)
+        {
+          cmm.Dispose();
+        }
+
+        cmm = null;
+      }
+    }
+
+    public void CleanUpFulfillmentByID(int p_intFulfillmentID)
+    {
+      SqlCommand cmm = new SqlCommand();
+      using (SqlConnection cnn = new SqlConnection(_MasterSource))
+      {
+        try
+        {
+          cnn.Open();
+
+          cmm.Connection = cnn;
+          cmm.CommandType = CommandType.StoredProcedure;
+          cmm.CommandText = "dbo.CleanUpFulfillmentByID";
+          cmm.CommandTimeout = 1000;
+          cmm.Parameters.Add("@FulfillmentID", SqlDbType.Int, -1).Value = p_intFulfillmentID;
+          cmm.ExecuteNonQuery();
+        }
+        finally
+        {
+          // No need to clean up  the connection since we are using 'using'.
+
+          if (cmm != null)
+          {
+            cmm.Dispose();
+          }
+
+          cmm = null;
+        }
+      }
+    }
+
+    public SqlDataReader GetCompressedFileData(int p_intFulfillmentID)
+    {
+      SqlCommand cmm = new SqlCommand();
+      SqlDataReader dr = null;
+      SqlConnection cnn = new SqlConnection(_MasterSource);
+
+      bool blnErrorOccurred = false;
+
+      try
+      {
+        cnn.Open();
+
+        cmm.Connection = cnn;
+        cmm.CommandType = CommandType.StoredProcedure;
+        cmm.CommandText = "dbo.GetCompressedFileData";
+
+        cmm.Parameters.Add("@FulfillmentID", SqlDbType.Int, -1).Value = p_intFulfillmentID;
+        dr = cmm.ExecuteReader(CommandBehavior.CloseConnection);
+
+        return dr;
+      }
+      catch (Exception ex)
+      {
+        blnErrorOccurred = true;
+
+        throw ex;
+      }
+      finally
+      {
+        if (blnErrorOccurred == true)
+        {
+          if (cnn != null)
+          {
+            if (cnn.State == ConnectionState.Open)
+            {
+              cnn.Close();
+            }
+          }
+
+          cnn = null;
+
+          if (!dr.IsClosed)
+          {
+            dr.Close();
+          }
+
+          dr = null;
+        }
+
+        if (cmm != null)
+        {
+          cmm.Dispose();
+        }
+
+        cmm = null;
+      }
+    }
+
+    public SqlDataReader GetFulfillmentIDs()
+    {
+      SqlCommand cmm = new SqlCommand();
+      SqlDataReader dr = null;
+      SqlConnection cnn = new SqlConnection(_MasterSource);
+
+      bool blnErrorOccurred = false;
+
+      try
+      {
+        cnn.Open();
+
+        cmm.Connection = cnn;
+        cmm.CommandType = CommandType.StoredProcedure;
+        cmm.CommandText = "dbo.GetFulfillmentIDs";
+        cmm.CommandTimeout = 1000;
+        dr = cmm.ExecuteReader(CommandBehavior.CloseConnection);
+
+        return dr;
+      }
+      catch (Exception ex)
+      {
+        blnErrorOccurred = true;
+
+        throw ex;
+      }
+      finally
+      {
+        if (blnErrorOccurred == true)
+        {
+          if (cnn != null)
+          {
+            if (cnn.State == ConnectionState.Open)
+            {
+              cnn.Close();
+            }
+          }
+
+          cnn = null;
+
+          if (!dr.IsClosed)
+          {
+            dr.Close();
+          }
+
+          dr = null;
+        }
+
+        if (cmm != null)
+        {
+          cmm.Dispose();
+        }
+
+        cmm = null;
+      }
+    }
+
+    public SqlDataReader GetFromDatabase(int p_intFulfillmentID, CommandBehavior p_cmdB)
+    {
+      SqlCommand cmm = new SqlCommand();
+      SqlDataReader dr = null;
+      SqlConnection cnn = new SqlConnection(_MasterSource);
+
+      bool blnErrorOccurred = false;
+
+      try
+      {
+        cnn.Open();
+
+        cmm.Connection = cnn;
+        cmm.CommandType = CommandType.StoredProcedure;
+        cmm.CommandText = "dbo.GetDataSentToCompress";
+        cmm.CommandTimeout = 1000;
+        cmm.Parameters.Add("@FulfillmentID", SqlDbType.Int, -1).Value = p_intFulfillmentID;
+        dr = cmm.ExecuteReader(p_cmdB);
+
+        return dr;
+      }
+      catch (Exception ex)
+      {
+        blnErrorOccurred = true;
+
+        throw ex;
+      }
+      finally
+      {
+        if (blnErrorOccurred == true)
+        {
+          if (cnn != null)
+          {
+            if (cnn.State == ConnectionState.Open)
+            {
+              cnn.Close();
+            }
+          }
+
+          cnn = null;
+
+          if (!dr.IsClosed)
+          {
+            dr.Close();
+          }
+
+          dr = null;
+        }
+
+        if (cmm != null)
+        {
+          cmm.Dispose();
+        }
+
+        cmm = null;
+      }
+    }
+  }
+}
